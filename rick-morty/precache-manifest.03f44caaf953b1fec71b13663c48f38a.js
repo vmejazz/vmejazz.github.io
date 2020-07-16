e75dab3c60029414cc72e11a7188133882c159d4
vmejazz <vmejazz@gmail.com>
2020-07-16 17:33:14 +0300

    Add rick and morty page

---
+++ rick-morty/precache-manifest.03f44caaf953b1fec71b13663c48f38a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d8eda57ccbb9510c4d556cf74178e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56a7a39c01b7dfd6e892",
+    "url": "/static/js/2.56d94eec.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.56d94eec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1552478ecd7699193bd",
+    "url": "/static/js/main.fafd0067.chunk.js"
+  },
+  {
+    "revision": "e1c9a40e84cc736387b5",
+    "url": "/static/js/runtime-main.f810d800.js"
+  }
+]);
